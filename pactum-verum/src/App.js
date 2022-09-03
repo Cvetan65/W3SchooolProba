8e36a025d95a498b00258961992f8c9c24712237
Cvetan Stojanovski <cvetan1965@gmail.com>
2022-09-03 23:40:47 +0200

    Add new component GitProba for prctice

--- pactum-verum/src/App.js
+++ pactum-verum/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import GitProba from './components/GitProba';
 import Header from './components/Header';
 
 function App() {
   return (
     <div className="App">
       <div>
+        <GitProba/>
          <Header/>
         App
       </div>
---
+++ pactum-verum/src/components/GitProba.js
@@ -0,0 +1,9 @@
+const GitProba = () => {
+    return ( 
+        <div>
+            Hello from GitProba in Git folder!
+        </div>
+     );
+}
+ 
+export default GitProba;
